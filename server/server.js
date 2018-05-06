c089830572b85a1ba14967924869876bc39e5f76
s10443 <104703043@nccu.edu.tw>
2018-05-06 14:56:13 +0800

    Add newMessage and createMessage events

--- server/server.js
+++ server/server.js
@@ -14,11 +14,21 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from: 'John',
+    text: 'See you then',
+    createdAt: 123123
+  });
+
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+  });
+
   socket.on('disconnect', () => {
     console.log('User was disconnected');
   });
 });
 
 server.listen(port, () => {
-  console.log(`Server is up on port ${port}`);
+  console.log(`Server is up on ${port}`);
 });
